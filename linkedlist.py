a65ebed6641e9ac9e82f0d7b09b726f74da7cd94
daemonvk18 <b121034@iiit-bh.ac.in>
2024-06-06 00:08:09 +0530

    added inset,remove,reverse methods

--- linkedlist.py
+++ linkedlist.py
@@ -68,28 +68,68 @@ class LinkedList:
         return temp.value 
 
     def get(self,index):
-        if self.length < index and index <0:
+        if self.length <= index or  index <0:
             return None
         temp = self.head
         count = 0
         while count != index:
             temp = temp.next
             count += 1
-        return temp       
+        return temp
+        #for _ in range(index):
+        #   temp = temp.next
+        #return temp  
+
+    def set_value(self,index,value):
+        temp = self.get(index)
+        if temp is not None:
+            temp.value = value
+            return True
+        return False
+
+    def insert(self,index,value):
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        if index <0 or index > self.length:
+            return False
+        newNode = Node(value)
+        temp = self.get(index-1)
+        newNode.next = temp.next
+        temp.next = newNode
+        self.length += 1
+        return True
+
+    def remove_node(self,index):
+        if index<0 or index>=self.length:
+            return None
+        if index == 0:
+            return self.popfirst()
+        if index == self.length-1:
+            return self.pop()
+        prev = self.get(index-1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp.value     
+
+    def reversell(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after    
                      
         
         
-my_Linked_list = LinkedList(1)
-# print(my_Linked_list.head.value)
-# print(my_Linked_list.tail.value)
-# print(my_Linked_list.length)
-# my_Linked_list.print_list()
+my_Linked_list = LinkedList(0)
+my_Linked_list.append(1)
 my_Linked_list.append(2)
-my_Linked_list.print_list()
-print(my_Linked_list.length)
-my_Linked_list.pop()
-#print(my_Linked_list.length)
-my_Linked_list.print_list()
-my_Linked_list.pop()
-print(my_Linked_list.pop())
-print(my_Linked_list.length)
+my_Linked_list.append(3)
