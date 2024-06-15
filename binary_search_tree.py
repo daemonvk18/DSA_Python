c220364755be6431c90a92d61176645cedac5424
daemonvk18 <b121034@iiit-bh.ac.in>
2024-06-16 01:08:01 +0530

    added constructors of bst

---
+++ binary_search_tree.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self,value) -> None:
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinarySeachTree:
+    def __init__(self):
+         self.root = None
+          
+
+
+my_binary_search_tree = BinarySeachTree()
+print(my_binary_search_tree.root)               
