00b5f5b3b0dc30f16799f81bb8198e9ce30d7c15
daemonvk18 <b121034@iiit-bh.ac.in>
2024-06-16 10:57:30 +0530

    added the insert and lookup methods

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -8,8 +8,49 @@ class Node:
 class BinarySeachTree:
     def __init__(self):
          self.root = None
+
+
+    def insert(self,value)->bool:
+        newNode = Node(value)
+        if self.root == None:
+            self.root = newNode
+            return True
+        temp = self.root
+        while (True):
+            if temp.value == newNode.value:
+                return False
+            if temp.value > newNode.value:
+                if temp.left is None:
+                    temp.left = newNode
+                    return True
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = newNode
+                    return True
+                temp = temp.right
+
+
+    def contains(self,value)->bool:
+        if self.root==None:return False
+        temp = self.root
+        while temp is not None:
+            if temp.value == value:return True
+            elif temp.value>value:
+                temp = temp.left
+            else:
+                temp = temp.right
+        return False            
+
           
 
 
 my_binary_search_tree = BinarySeachTree()
-print(my_binary_search_tree.root)               
+my_binary_search_tree.insert(47)
+my_binary_search_tree.insert(21)
+my_binary_search_tree.insert(18)
+my_binary_search_tree.insert(27)
+my_binary_search_tree.insert(52)
+my_binary_search_tree.insert(84)
+print(my_binary_search_tree.contains(90))
+              
