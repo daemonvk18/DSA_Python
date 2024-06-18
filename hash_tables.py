b8f2414f814d5f646bf350ab4c3bb966ff09bfdd
daemonvk18 <b121034@iiit-bh.ac.in>
2024-06-19 00:01:55 +0530

    completed hashtables

--- hash_tables.py
+++ hash_tables.py
@@ -17,7 +17,24 @@ class HashTable:
         index = self._hash(key)
         if self.data_map[index] == None:
             self.data_map[index] = []
-        self.data_map[index].append([key,value])    
+        self.data_map[index].append([key,value])
+
+    def get_item(self,key):
+        index = self._hash(key)
+        if self.data_map[index] is not None:
+            for i in self.data_map[index]:
+                if i[0] == key:
+                    return i[1]
+        return None
+
+
+    def get_keys(self):
+        all_keys  = []
+        for i in range(len(self.data_map)):
+            if self.data_map[i] is not None:
+                for j in range(len(self.data_map[i])):
+                    all_keys.append(self.data_map[i][j][0])
+        return all_keys                           
 
 
 
@@ -25,4 +42,6 @@ my_hash_table = HashTable()
 my_hash_table.set_item("bolts",1400)
 my_hash_table.set_item("washers",50)
 my_hash_table.set_item("lumber",1000)
-my_hash_table.print_hashtable()                    
+print(my_hash_table.get_item("bolts"))
+print(my_hash_table.get_item("preetham")) 
+print(my_hash_table.get_keys())                  
