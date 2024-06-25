0a9ef76cb9b0927683fa83460ae37f9b313954c6
daemonvk18 <b121034@iiit-bh.ac.in>
2024-06-25 17:55:56 +0530

    added the reverse_string and sort_stack methods

--- stacks_leetcode_questions.py
+++ stacks_leetcode_questions.py
@@ -19,14 +19,45 @@ class Stack:
         else:
             return self.stack_list.pop()
         
+    def peek_element(self):
+        if self.is_empty():
+            return None
+        return self.stack_list[-1]  
+        
+
+    def reverse_string(self,s)->str:
+        stack = Stack()
+        reversed_string = ""
+        for char in s:
+            stack.push_element(char)
+
+        while not stack.is_empty():
+            reversed_string += stack.pop_element()
+        return reversed_string
+
+
+    def sort_stack(self,old_stack):
+        new_stack = Stack()
+        while not old_stack.is_empty():
+            temp = old_stack.pop_element()
+            while not new_stack.is_empty() and new_stack.peek_element()>temp:
+                old_stack.push_element(new_stack.pop_element())
+
+            new_stack.push_element(temp)
+        return new_stack                       
+
+
+
+
+        
             
 
 
 my_stack = Stack()
-my_stack.push_element(11)
-my_stack.push_element(13)
+my_stack.push_element(5)
+my_stack.push_element(1)
+my_stack.push_element(3)
 my_stack.push_element(2)
 my_stack.push_element(6)
-my_stack.print_stack()
-poped_element = my_stack.pop_element()
-print(poped_element)        
+new_stack = my_stack.sort_stack(my_stack)
+new_stack.print_stack()
