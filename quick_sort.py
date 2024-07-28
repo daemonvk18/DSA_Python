98a552b78c0faa612e38fe50485b40c962ba7dd4
daemonvk18 <b121034@iiit-bh.ac.in>
2024-07-28 20:23:24 +0530

    added the quick_sort method

---
+++ quick_sort.py
@@ -0,0 +1,23 @@
+def pivot_finder(my_list,pivot_index,end_index):
+    swap_index = pivot_index
+    for i in range(pivot_index+1,end_index+1):
+        if my_list[i]<my_list[pivot_index]:
+            swap_index+=1
+            swap(my_list,swap_index,i)
+    swap(my_list,swap_index,pivot_index)
+    return swap_index
+
+def swap(my_list,index1,index2):
+    temp = my_list[index1]
+    my_list[index1] = my_list[index2]
+    my_list[index2] = temp
+
+def quick_sort(my_list,left,right):
+    if left<right:
+      pivot_index = pivot_finder(my_list,left,right)
+      pivot_finder(my_list,left,pivot_index-1)
+      pivot_finder(my_list,pivot_index+1,right)
+    return my_list
+
+print(quick_sort([4,6,1,7,3,2,5],0,6))             
+
